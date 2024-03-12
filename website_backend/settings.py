e9c23db02ddde0badcf94369e327adf921edc6a9
WRFMIXO <jakim2621@gmail.com>
2024-03-12 21:47:19 +0100

    update

--- website_backend/settings.py
+++ website_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(l=bb-_jofgseztte^2fz&sx7c(%(_a6f@e%ttf7ntjax2gtra
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '16.171.142.69']
 
 
 # Application definition
