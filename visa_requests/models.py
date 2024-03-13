8066e13b00ba873dc27478af1b8d3e1d44fe8b5a
WRFMIXO <jakim2621@gmail.com>
2024-03-13 17:08:15 +0100

    modifiyng visa requests serializer

--- visa_requests/models.py
+++ visa_requests/models.py
@@ -4,6 +4,13 @@ from utilisateurs.models import CustomUser
 # Create your models here.
 class VisaRequests(models.Model):
 
+    STATUSES = [
+        ('evaluation', 'Etude en cours'),
+        ('approved', 'Approuvées'),
+        ('rejected', 'Rejetées'),
+    ]
+
+    status = models.CharField(max_length=255, choices=STATUSES, default='evaluation')
     code = models.CharField(max_length=30)
     creation_date = models.DateField(auto_now_add=True)
     reason = models.TextField()
